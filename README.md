d359517dc2973d5a18ac56ea217dacbb07b853d6
Quentin Rodrigues <quentin.rodrigues1912@gmail.com>
2023-10-21 17:02:05 +0200

    feat: algorithmie 1/2 et 2/2 + scrapping 2/2

--- README.md
+++ README.md
@@ -15,3 +15,24 @@ Les trois chaînes les plus diffusées sont : Netflix avec 108, Disney+ avec 34 
 Faire de même pour les pays (pensez à mutualiser votre code !)
 ```
 Les trois pays les plus diffusés sont : Etats-Unis avec 355, France avec 76 et Canada avec 63.
+
+```
+Quels sont les 10 mots les plus présents dans les noms des séries ? 
+```
+1 - "The" utilisé 66 fois.  
+2 - "of" utilisé 31 fois.  
+3 - "de" utilisé 24 fois.  
+4 - "(2023)" utilisé 19 fois.  
+5 - "Pacto" utilisé 18 fois.  
+6 - "Silencio" utilisé 18 fois.  
+7 - "Les" utilisé 17 fois.  
+8 - "the" utilisé 16 fois.  
+9 - "(UK)" utilisé 12 fois.  
+10 - "Everything" utilisé 11 fois.
+
+
+ # Algorithmie [2/2]
+```
+Quelle est la chaîne de TV qui diffuse des épisodes pendant le plus grand nombre de jours consécutifs sur le mois d’Octobre ? 
+```
+Il y a beaucoup de chaîne de TV qui diffuse 2 jours consécutifs, c'est le maximum et le premier à la faire au mois d'octbre est NBC.
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from webscrapper import ScrapEpisodes
 from Algorithmie.Algorithmie import Algorithmie
 
-scrapper = ScrapEpisodes("https://www.spin-off.fr/calendrier_des_series.html")
+base_url = "https://www.spin-off.fr"
+
+scrapper = ScrapEpisodes(base_url, "/calendrier_des_series.html")
 episodes = scrapper.getAllEpisodes()
 
 for episode in episodes:
     print(episode)
 
 algorithmie = Algorithmie()
-diffusions_channels_counter = algorithmie.countDiffusionByKey(episodes, "channel")
-top_3_channels = algorithmie.getTop3(diffusions_channels_counter, 3)
-
-diffusions_countries_counter = algorithmie.countDiffusionByKey(episodes, "origin_country")
-top_3_countries = algorithmie.getTop3(diffusions_countries_counter, 3)
 
 print('\n---- Nombre de difussion(s) par chaîne ----\n')
+diffusions_channels_counter = algorithmie.countDiffusionByKey(episodes, "channel")
+top_3_channels = algorithmie.getTop3(diffusions_channels_counter, 3)
 print(diffusions_channels_counter)
 print('\nTop 3 des chaînes les plus diffusées : ', top_3_channels)
 
 print('\n---- Nombre de difussion(s) par pays ----\n')
+diffusions_countries_counter = algorithmie.countDiffusionByKey(episodes, "origin_country")
+top_3_countries = algorithmie.getTop3(diffusions_countries_counter, 3)
 print(diffusions_countries_counter)
 print('\nTop 3 des pays les plus diffusées : ', top_3_countries)
+
+print('\n---- 10 mots les plus présents dans les noms de séries ----')
+top_10_words = algorithmie.getTopSeriesWords(episodes, 10)
+for index, word in enumerate(top_10_words):
+    print(f"{index + 1} - \"{word[0]}\" utilisé {word[1]} fois.")
+
+print('\n----  La chaîne de TV qui diffuse des épisodes pendant le plus grand nombre de jours consécutifs ----')
+chaine_consecutive = scrapper.findLongestConsecutiveDays(episodes)
+print(f"La chaîne de TV qui diffuse des épisodes pendant le plus grand nombre de jours consécutifs est {chaine_consecutive[0]} avec {chaine_consecutive[1]} diffusions.")
